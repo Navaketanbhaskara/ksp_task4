dded86137efba9cabf1e52bfd8549a96c2fb478b
Navaketanbhaskara <127087051+Navaketanbhaskara@users.noreply.github.com>
2024-02-25 22:52:49 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+let name="Navaketan";
+let char="B"
+let number1=8.5;
+let number2=55;
+console.log(number2);
+const number3=33;
+console.log(number3);
+number4=55;
+number4=44;
+console.log(number4);
+let bool=true;
+let name1="Navaketan";
+console.log(typeof(name1));
+let char1="B"
+console.log(typeof(bool));
+let number=10;
+console.log(typeof(number));
+console.log(typeof(char1));
